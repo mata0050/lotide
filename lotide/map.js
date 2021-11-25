2b89a402e402c1a660c7193a742248b0956a0065
mata0050 <mata0050@outlook.com>
2021-11-25 14:27:42 -0500

    map.js

---
+++ lotide/map.js
@@ -0,0 +1,37 @@
+const words = ['ground', 'control', 'to', 'major', 'tom'];
+
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, (word) => word[0]);
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    console.log(false);
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(false);
+      return false;
+    }
+  }
+  console.log(true);
+  return true;
+};
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Fail: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual(eqArrays(results1, ['g', 'c', 't', 'm', 't']), true);
