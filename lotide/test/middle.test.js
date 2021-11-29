ecc110b45181f4091ae21198480b8d4ed0968221
mata0050 <mata0050@outlook.com>
2021-11-29 18:55:05 -0500

    tests

--- lotide/test/middle.test.js
+++ lotide/test/middle.test.js
@@ -1,12 +1,28 @@
-const assertEqual = require('../assertEqual');
 const middle = require('../middle');
 
-// TEST CODE
-assertEqual(middle([1]), `[]`);
-assertEqual(middle([1, 2]), `[]`);
+const expect = require('chai').expect;
 
-assertEqual(middle([1, 2, 3]), `[2]`);
-assertEqual(middle([1, 2, 3, 4, 5]), `[3]`);
+describe('#middle', () => {
+  it(`returns '[]' for [1]`, () => {
+    expect(middle([1])).to.eql(`[]`);
+  });
 
-assertEqual(middle([1, 2, 3, 4]), `[2, 3]`);
-assertEqual(middle([1, 2, 3, 4, 5, 6]), `[3, 4]`);
+  it(`returns '[2]' for [middle([1, 2, 3])`, () => {
+    expect(middle([1, 2, 3])).to.eql(`[2]`);
+  });
+
+  it(`returns '[3]' for [1,2,3,4,5]`, () => {
+    expect(middle([1, 2, 3, 4, 5])).to.eql(`[3]`);
+  });
+
+  it(`returns '[2,3]' for [1,2,3,4]`, () => {
+    expect(middle([1, 2, 3, 4])).to.eql(`[2,3]`);
+  });
+
+  it(`returns '[3,4]' for [1,2,3,4,5,6]`, () => {
+    expect(middle([1])).to.eql(`[]`);
+  });
+});
+
+
+
--- lotide/test/tail.test.js
+++ lotide/test/tail.test.js
@@ -1,8 +1,9 @@
-const assertEqual = require('../assertEqual');
+const expect = require('chai').expect;
 const tail = require('../tail');
 
-// TEST CODE
-const result = tail(['Hello', 'Lighthouse', 'Labs']);
-assertEqual(result, ['Lighthouse', 'Labs']);
-
-tail([5, 3, 2]);
+describe('#tail', () => {
+  it(`returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']`, () => {
+    const result = tail(['Hello', 'Lighthouse', 'Labs']);
+    expect(result).to.eql(['Lighthouse', 'Labs']);
+  });
+});
